0ec3f012d8f5a2888f2ff3c4bc09262a5aed3748
DiegoxK <diego.stv@outlook.es>
2025-06-11 20:27:23 -0500

    refactor folders part2

--- src/app/(routes)/chat/[[...chatId]]/page.tsx
+++ src/app/(routes)/chat/[[...chatId]]/page.tsx
@@ -1,3 +1,16 @@
-export default function ChatId() {
-  return <div>ChatId</div>;
+interface ChatPageProps {
+  params: Promise<{
+    chatId?: string[];
+  }>;
+}
+export default async function ChatPage({ params }: ChatPageProps) {
+  const resolvedParams = await params;
+  const chatId = resolvedParams.chatId?.[0];
+
+  return (
+    <div>
+      ChatId
+      <span>{chatId}</span>
+    </div>
+  );
 }
