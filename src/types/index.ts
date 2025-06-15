1e1041a6a9fb9ed88fc7c9b79298aed84cbdc074
DiegoxK <diego.stv@outlook.es>
2025-06-15 11:37:19 -0500

    update Message type to align with chat router outputs

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
-import type { z } from "zod";
 import { type RouterOutputs } from "@/trpc/react";
 
 import { type users } from "@/server/db/schema";
 
 export type User = typeof users.$inferSelect;
+export type Message = RouterOutputs["chat"]["getMessages"][number];
